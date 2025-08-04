b2e24eadcb25e9989a76b7487bd7659fc01cf673
shymaaismail <shymaa.m.ismail@gmail.com>
2025-08-04 21:53:41 +0300

    deploy: add build.sh file

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Exit if any command fails
+set -o errexit
+
+echo "🔧 Using Python version  d ddddd: $(python --version)"
+
+# Upgrade pip and setup tools
+pip install --upgrade pip setuptools wheel
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files (if you use them)
+python manage.py collectstatic --noinput
+
+# Apply migrations (optional if you want to run them during build)
+python manage.py migrate --noinput
