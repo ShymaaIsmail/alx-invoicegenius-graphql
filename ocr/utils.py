eb290e43cde33cfddecf3fb7828833453e812d6f
shymaamismail <shymaa.m.ismail@gmail.com>
2025-08-06 13:59:37 +0300

    fix: clean files conflicts

--- ocr/utils.py
+++ ocr/utils.py
@@ -1,31 +1,3 @@
-
-import os
-import fitz  # PyMuPDF
-from PIL import Image
-import pytesseract
-
-
-def extract_text_from_pdf(file_path):
-    """Extract text from a PDF file using PyMuPDF."""
-    from django.core.files.storage import default_storage
-
-    text = ""
-
-    full_path = file_path if os.path.isabs(file_path) else default_storage.path(file_path)
-
-    with fitz.open(full_path) as doc:
-        for page in doc:
-            text += page.get_text()
-    return text
-
-
-def extract_text_from_image(file_path):
-    """Extract text from an image file using Tesseract OCR."""
-    full_path = file_path if os.path.isabs(file_path) else default_storage.path(file_path)
-
-    image = Image.open(full_path)
-    return pytesseract.image_to_string(image)
-=======
 import os
 import fitz  # PyMuPDF
 from PIL import Image
