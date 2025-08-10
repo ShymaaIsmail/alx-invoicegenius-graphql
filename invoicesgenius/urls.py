bfdf25599545dba3a7455efd13986fe3df537f3f
shymaamismail <shymaa.m.ismail@gmail.com>
2025-08-10 07:50:28 +0300

    fix: handle url / to redirect to graphql

--- invoicesgenius/urls.py
+++ invoicesgenius/urls.py
@@ -20,11 +20,13 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_file_upload.django import FileUploadGraphQLView
+from django.views.generic.base import RedirectView
 
 """GraphQL endpoint for handling file uploads and queries"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("graphql/", csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True))),
+    path('', RedirectView.as_view(url='/graphql/', permanent=False)),
 
 ]
 
